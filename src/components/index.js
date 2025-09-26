8da6c52e10ec78f0eccb19484058aa3bd9b3962c
Hassan <hassannawaz1420@gmail.com>
2025-09-26 09:47:50 +0500

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import PostCard from "./PostCard";
+import RTE from "./RTE";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import PostForm from "./postForm/PostForm";
+import Protected from "./AuthLayout";
+import Input from "./Input";
+import Select from "./Select";
+export {
+    Header ,
+    Footer ,
+    Container ,
+    Logo ,
+    LogoutBtn ,
+    RTE ,
+    Signup ,
+    Login ,
+    Button ,
+    PostForm ,
+    PostCard ,
+    Protected ,
+    Input ,
+   Select ,   
+}
